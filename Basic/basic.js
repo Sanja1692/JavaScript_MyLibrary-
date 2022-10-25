054cd9285817d83eb9f39b2f79e55c04d5ad8a94
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-10-25 22:50:02 +0200

    js

--- Basic/basic.js
+++ Basic/basic.js
@@ -481,4 +481,20 @@ function testGreaterThan(val) {
     return "10 or Under";
 }
 testGreaterThan(10);
-//check
+
+//Comparison with the Greater Than Or Equal To Operator
+// >=
+
+function testGreaterOrEqual(val) {
+    if (val >= 20) { // Change this line
+        return "20 or Over";
+    }
+
+    if (val >= 10) { // Change this line
+        return "10 or Over";
+    }
+
+    return "Less than 10";
+}
+
+testGreaterOrEqual(10);
