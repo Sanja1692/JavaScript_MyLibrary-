dfc6bcd6b8202f765317ff32171b194becb6aac7
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-10-26 10:59:46 +0200

    golf

--- Basic/basic.js
+++ Basic/basic.js
@@ -529,3 +529,23 @@ function testElse(val) {
     return result;
 }
 testElse(4);
+//Introducing Else If Statements
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+    testElseIf(7);
+}
+//Golf codeconst names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+    // Only change code below this line
+    if (strokes == 1) { return "Hole-in-one!"; } else if (strokes <= par - 2) { return names[1]; } else if (strokes == par - 1) { return names[2]; } else if (strokes == par) { return names[3]; } else if (strokes == par + 1) { return "Bogey"; } else if (strokes == par + 2) { return "Double Bogey"; } else if (strokes >= par + 3) { return "Go Home!"; } else { return "Change Me"; }
+    // Only change code above this line
+}
+
+golfScore(5, 4);
