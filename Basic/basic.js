4b9dccac20679eca63fe9cb201b06f1a5117622e
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-10-26 23:42:09 +0200

    d

--- Basic/basic.js
+++ Basic/basic.js
@@ -574,3 +574,65 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1);
+//Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    //Returning Boolean Values from Functions:
+    //1
+    function isLess(a, b) {
+        // Only change code below this line
+        if (a < b) {
+            return true;
+        } else {
+            return false;
+        }
+        // Only change code above this line
+    }
+    isLess(10, 15); //
+    //2
+    function isLess(a, b) {
+        // Only change code below this line
+        return a < b;
+        // Only change code above this line
+    }
+    isLess(10, 15);
+    //3
+    function isLess(a, b) {
+        // Only change code below this line
+        return a < b;
+        // Only change code above this line
+    }
+    isLess(20, 15);
+    // Only change code above this line
+    return answer;
+}
+sequentialSizes(1);
+//Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0) {
+        return undefined; // if a or b are less than 0 the function will immediately exit with a value of undefined.
+    }
+    // Only change code above this line
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+abTest(2, 2);
