79a72660dea82b4920fad7c401c172844f18a50f
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-10-31 22:13:55 +0100

    dot

--- Basic/basic.js
+++ Basic/basic.js
@@ -677,4 +677,22 @@ const cat = {
     "tails": 1,
     "enemies": ["Water", "Dogs"]
 };
-//Accessing Object Properties with Dot Notation
+//Accessing Object Properties with Dot Notation - Dot notation is what you use when you know the name of the property you're trying to access ahead of time.// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
+
+//or
+const myDotNotation = {
+    prop1: "val1",
+    prop2: "val2"
+};
+
+const prop1val = myDotNotation.prop1;
+const prop2val = myDotNotation.prop2;
