d8ebec16295feb76675f6529484fee72198ab435
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-01 22:23:43 +0100

    j

--- Basic/basic.js
+++ Basic/basic.js
@@ -742,3 +742,40 @@ const myDog = {
 };
 // Only change code below this line
 delete myDog.tails;
+
+//Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    const alpha = {
+        1: "Z",
+        2: "Y",
+        3: "X",
+        4: "W",
+        24: "C",
+        25: "B",
+        26: "A"
+    };
+
+    const thirdLetter = alpha[2];
+    const lastLetter = alpha[24];
+
+    const value = 2;
+    const valueLookup = alpha[value];
+    //test
+    const lookup = {
+        "alpha": result = "Adams",
+        "bravo": result = "Boston",
+        "charlie": result = "Chicago",
+        "delta": result = "Denver",
+        "echo": result = "Easy",
+        "foxtrot": result = "Frank"
+    };
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
