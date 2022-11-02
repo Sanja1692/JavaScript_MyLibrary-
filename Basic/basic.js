12af4e5ab65c2500299b6613497f575a27aa37c1
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-02 21:47:04 +0100

    more objects

--- Basic/basic.js
+++ Basic/basic.js
@@ -781,3 +781,34 @@ function phoneticLookup(val) {
 phoneticLookup("charlie");
 
 //Testing Objects for Properties - check if the property of a given object exists or not.
+function checkObj(obj, checkProp) { //Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp). If the property is found, return that property's value. If not, return "Not Found".
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+    } else {
+        return "Not Found";
+    }
+    // Only change code above this line
+}
+//Manipulating Complex Objects
+const myMusic = [{
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+        "CD",
+        "8T",
+        "LP"
+    ],
+    "gold": true
+}, {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+    "formats": [
+        "CD",
+        "Cassette",
+        "LP"
+    ],
+    "gold": true
+}];
