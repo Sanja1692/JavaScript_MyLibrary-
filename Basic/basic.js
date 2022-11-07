1460d5f5a73515a61183be6f3c82f0f30c120c0e
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-11-07 21:52:19 +0100

    while

--- Basic/basic.js
+++ Basic/basic.js
@@ -900,10 +900,16 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
-    if (prop != "tracks" && value != " ") {
-        recordCollection.prop = value;
+    if (prop !== 'tracks' && value !== "") {
+        records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+        records[id][prop].push(value);
+    } else if (value === "") {
+        delete records[id][prop];
     }
     return records;
 }
 
-updateRecords(recordCollection, 5439, 'artist', 'ABBA'); //NOT FINISHED
+//Iterate with JavaScript While Loops
